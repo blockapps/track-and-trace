5d3b8446b6079e5f15dc7276b2783b91821f415d
Samrit Sangal <samrit@samrit.io>
2019-01-23 12:15:56 -0500

    Bootstrap

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const baseUrl = 'api/v1';
+const package = require('./package.json');
+const moment = require('moment');
+
+app.get(`${baseUrl}`, (req, res) => {
+  res.json({
+    name: package.name,
+    description: package.description,
+    version: package.version,
+    timestamp: moment().unix()
+  })
+})
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => console.log(`Listening on ${port}`));

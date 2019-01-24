f7d9c298075cee5263157bc4bb7c0f4c861e4607
Zach Yuzon <zyuzon24@gmail.com>
2019-01-24 12:50:40 -0500

    Update TtPermission Contracts

---
+++ server/dapp/ttPermission/contracts/TtPermissionManager.sol
@@ -0,0 +1,37 @@
+import "/blockapps-sol/auth/permission/contracts/PermissionManager.sol";
+import "./TtPermission.sol";
+import "./TtRolePermissions.sol";
+
+/**
+* TT Permission Manager
+*/
+contract TtPermissionManager is PermissionManager, TtPermission, TtRolePermissions {
+
+  constructor(
+    address _admin,
+    address _master) public
+    PermissionManager(_admin, _master) {
+    // grant Tt Admin permissions to admin
+    grantRole('Admin', _admin, TtRole.ADMIN);
+  }
+
+  function grantRole(string _id, address _address, TtRole _role) public returns (uint, uint) {
+    uint permissions = getRolePermissions(_role);
+    return grant(_id, _address, permissions);
+  }
+
+  function canCreateAsset(address _address) returns (bool) {
+    uint permissions = 1 << uint(TtPermission.CREATE_ASSET);
+    return check(_address, permissions) == RestStatus.OK;
+  }
+
+  function canTransferOwnershipMap(address _address) returns (bool) {
+    uint permissions = 1 << uint(TtPermission.TRANSFER_OWNERSHIP_MAP);
+    return check(_address, permissions) == RestStatus.OK;
+  }
+
+  function canCreateUser(address _address) returns (bool) {
+    uint permissions = 1 << uint(TtPermission.CREATE_USER);
+    return check(_address, permissions) == RestStatus.OK;
+  }
+}
--- server/dapp/ttPermission/contracts/TtRolePermission.sol
+++ server/dapp/ttPermission/contracts/TtRolePermission.sol
@@ -3,3 +3,33 @@ ONLY the manufacturer can create assets
 ONLY the admin can create users
 ONLY the manufacturer and the distributor can transfer ownership
 */
+import "./TtRole.sol";
+import "./TtPermission.sol";
+
+contract TtRolePermission  is TtRole, TtPermission {
+  uint[] rolePermissions;
+  constructor() {
+    rolePermissions.length = uint(TtRole.REGULATOR)+1;
+    rolePermissions[uint(TtRole.NULL)] = 0;
+
+    rolePermissions[uint(TtRole.ADMIN)] =
+      (1 << uint(TtPermission.CREATE_USER)) ;
+
+    rolePermissions[uint(TtRole.MANUFACTURER)] =
+      (1 << uint(TtPermission.TRANSFER_OWNERSHIP_MAP)) |
+      (1 << uint(TtPermission.CREATE_ASSET)) ;
+
+    rolePermissions[uint(TtRole.DISTRIBUTOR)] =
+      (1 << uint(TtPermission.TRANSFER_OWNERSHIP_MAP)) ;
+
+    rolePermissions[uint(TtRole.RETAILER)] =
+      (1 << uint(EchoPermission.TRANSFER_OWNERSHIP_MAP)) ;
+
+    rolePermissions[uint(EchoRole.OFFICER)] =
+      (1 << uint(EchoPermission.GAS_MODIFY_DEAL));
+  }
+
+  function getRolePermissions(TtRole _role) returns (uint) {
+    return rolePermissions[uint(_role)];
+  }
+}

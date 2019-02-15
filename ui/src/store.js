e0869957e3c813afe9fbd9d711df5d66149c12fd
Tanuj Soni <tanuj@blockapps.net>
2019-02-15 11:51:57 +0530

    Console logger for redux

--- ui/src/store.js
+++ ui/src/store.js
@@ -33,6 +33,7 @@ if (process.env.NODE_ENV === 'development') {
     // ...options
   })
 
+  middleware.push(logger);
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
 
   if (typeof devToolsExtension === 'function') {

77ebe4a5f4626993fcd91765d932c6dff5c1bc51
Tanuj Soni <tanuj@blockapps.net>
2019-02-21 18:15:05 +0530

    [TT-27_bidAPI] API: transferOwnership

--- server/api/v1/assets/index.js
+++ server/api/v1/assets/index.js
@@ -6,6 +6,7 @@ router.get('/', controller.getAssets);
 router.get('/:sku', controller.getAsset);
 router.post('/', controller.createAsset);
 router.post('/handleEvent', controller.handleAssetEvent);
+router.post('/transferOwnership', controller.transferOwnership);
 
 
 module.exports = router;

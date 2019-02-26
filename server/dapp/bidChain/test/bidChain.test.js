5193d8e5a3eff5559ffa41c99ce972a16564352c
Zach Yuzon <zyuzon24@gmail.com>
2019-02-26 11:57:28 -0500

    [TT-28_Complete_BidChain_Tests] Tests Passing

--- server/dapp/bidChain/test/bidChain.test.js
+++ server/dapp/bidChain/test/bidChain.test.js
@@ -5,13 +5,9 @@ const { assert, config, fsutil, util } = common;
 const { getEmailIdFromToken, createStratoUser } = require(`${process.cwd()}/helpers/oauth`);
 
 const bidChain = require(`${process.cwd()}/${config.dappPath}/bidChain/bidChain`);
-const bidJs = require(`${process.cwd()}/${config.dappPath}/bid/bid`);
-const ttPermissionManagerJs = require(`${process.cwd()}/${config.dappPath}/ttPermission/ttPermissionManager`);
-const assetFactory = require(`${process.cwd()}/${config.dappPath}/asset/asset.factory`);
-const assetManagerJs = require(`${process.cwd()}/${config.dappPath}/asset/assetManager`);
 
 const contractName = 'BidGovernance';
-const contractAddress = '0000000000000000000000000000000000000100';   // the governance contract on private chains has this same address
+const contractFileName = `${process.cwd()}/${config.dappPath}/bidChain/contracts/BidChain.sol`;
 
 describe('Bid Chain Tests', function() {
   this.timeout(60*1000);
@@ -19,27 +15,12 @@ describe('Bid Chain Tests', function() {
   const distributorToken = process.env.DISTRIBUTOR_TOKEN;
   const manufacturerToken = process.env.MANUFACTURER_TOKEN;
   const adminToken = process.env.ADMIN_TOKEN;
-  const masterToken = process.env.MASTER_TOKEN;
-  let assetManagerContract, manufacturerAssetManagerContract;
 
   function* createUser(userToken) {
     const userEmail = getEmailIdFromToken(userToken);
     const createAccountResponse = yield createStratoUser(userToken, userEmail);
     assert.equal(createAccountResponse.status, 200, createAccountResponse.message);
     return { address: createAccountResponse.address, username: userEmail };
-  }
-
-  function* createAsset() {
-    const assetArgs = assetFactory.getAssetArgs();
-    const asset = yield manufacturerAssetManagerContract.createAsset(assetArgs);
-    assert.equal(asset.sku, assetArgs.sku, 'sku');
-    return asset;
-  }
-
-  function bindAssetManagerContractToUser(user, contract) {
-    let copy = Object.assign({}, contract);
-
-    return assetManagerJs.bind(user, copy);
   }
 
   before(function* () {
@@ -49,14 +30,6 @@ describe('Bid Chain Tests', function() {
     manufacturerUser = yield createUser(manufacturerToken);
     distributorUser = yield createUser(distributorToken);
     adminUser = yield createUser(adminToken);
-
-    const ttPermissionManager = yield ttPermissionManagerJs.uploadContract(adminToken, masterToken);
-    assetManagerContract = yield assetManagerJs.uploadContract(adminToken, ttPermissionManager);
-
-    yield ttPermissionManager.grantManufacturerRole(manufacturerUser);
-    yield ttPermissionManager.grantDistributorRole(distributorUser);
-
-    manufacturerAssetManagerContract = bindAssetManagerContractToUser(manufacturerToken, assetManagerContract);
   })
 
   it('should create a bidding chain', function* () {
@@ -81,101 +54,74 @@ describe('Bid Chain Tests', function() {
   })
 
   // TODO: test does not pass. member is not being removed.
-  it.skip('should remove a member from bidding chain', function* () {
-    // const chainId = yield bidChain.createChain(distributorToken, manufacturerUser.address)
-    // // takes a few to populate
-    // yield util.sleep(2*1000);
-    //
-    // const chain = yield bidChain.getChainById(chainId);
-    // console.log(chain.members);
-    const asset = yield createAsset();
+  it('should remove a member from bidding chain', function* () {
+    const chainId = yield bidChain.createChain(distributorToken, manufacturerUser.address)
+    // takes a few to populate
+    yield util.sleep(2*1000);
 
-    const bidValue = 100;
-    const bid = yield bidJs.createBid(distributorToken, asset.address, asset.owner, bidValue);
-    const chain = yield bidChain.getChainById(bid.chainId);
-    console.log('HERE IS THE CHAIN', chain);
-    console.log('HERE IS THE MANUFACTURER USER', manufacturerUser);
+    const chain = yield bidChain.getChainById(chainId);
     assert.equal(chain.label, `bid_${distributorUser.address}_${manufacturerUser.address}`, 'Chain label should match');
     assert.equal(chain.members.length, 2, 'Chain should have 2 members');
 
-
-
-    const bidContract = bidChain.bind(distributorToken, {
-        name: 'Bid',
-        address: bid.address,
-        src: 'removed'
-      },
-      bid.chainId
+    const govContract = yield rest.uploadContract(
+      distributorToken,
+      contractName,
+      contractFileName,
+      {},
+      {
+        chainId: chainId
+      }
     );
-    // bidContract = bidChain.bind(distributorToken, {
-    //   name: 'Bid',
-    //   address: bidContract.address,
-    //   src: 'removed'
-    // });
-    console.log('here is the bidContract', bidContract);
-    const result = bidContract.removeMember(manufacturerUser.address);
-    // const result = yield bidChain.removeMember(distributorToken,
-    //   {
-    //     name: 'Bid',
-    //     address: bidContract.address,
-    //     src: 'removed'
-    //   },
-    //   manufacturerUser.address,
-    //   {
-    //     chainId: chainId
-    //   }
-    // );
-    console.log('Here is the result', result);
-    const updatedChain = yield bidChain.getChainById(bid.chainId);
+
+    const result = yield bidChain.removeMember(
+      distributorToken,
+      {
+        name: contractName,
+        address: govContract.address
+      },
+      manufacturerUser.address,
+      chainId
+    );
+
+    const updatedChain = yield bidChain.getChainById(chainId);
     assert.equal(updatedChain.label, `bid_${distributorUser.address}_${manufacturerUser.address}`, 'Chain label should match');
     assert.equal(updatedChain.members.length, 1, 'Chain should have 1 member');
-
   })
 
   it('should add a member from bidding chain', function* () {
-    // const chainId = yield bidChain.createChain(distributorToken, manufacturerUser.address)
-    //
-    // // takes a few to populate
-    // yield util.sleep(2*1000);
-    const asset = yield createAsset();
+    const chainId = yield bidChain.createChain(distributorToken, manufacturerUser.address)
 
-    const bidValue = 100;
-    const bid = yield bidJs.createBid(distributorToken, asset.address, asset.owner, bidValue);
-    const chain = yield bidChain.getChainById(bid.chainId);
-    console.log('HERE IS THE CHAIN', chain);
-    console.log('HERE IS THE MANUFACTURER USER', manufacturerUser);
+    // takes a few to populate
+    yield util.sleep(2*1000);
+
+    const chain = yield bidChain.getChainById(chainId);
     assert.equal(chain.label, `bid_${distributorUser.address}_${manufacturerUser.address}`, 'Chain label should match');
     assert.equal(chain.members.length, 2, 'Chain should have 2 members');
 
-
-
-    const bidContract = bidChain.bind(distributorToken, {
-        name: 'Bid',
-        address: bid.address,
-        src: 'removed'
-      },
-      bid.chainId
+    const govContract = yield rest.uploadContract(
+      distributorToken,
+      contractName,
+      contractFileName,
+      {},
+      {
+        chainId: chainId
+      }
     );
 
+    const result = yield bidChain.addMember(
+      distributorToken,
+      {
+        name: contractName,
+        address: govContract.address
+      },
+      adminUser.address,
+      chainId
+    );
 
-    console.log('Here is adminUser to be added', adminUser);
-    // const result = yield bidChain.addMember(distributorToken,
-    //   {
-    //     name: contractName,
-    //     address: contractAddress
-    //   },
-    //   adminUser.address,
-    //   {
-    //     chaindId: chainId
-    //   }
-    // );
-    const result = bidContract.addMember(adminUser.address);
-    console.log('HERE IS THE RESULT', result);
+    const updatedChain = yield bidChain.getChainById(chainId);
 
-    const newChain = yield bidChain.getChainById(bid.chainId);
-    console.log('THIS IS THE NEW CHAIN', newChain);
-    assert.equal(newChain.label, `bid_${distributorUser.address}_${manufacturerUser.address}`, 'Chain label should match')
-    assert.equal(newChain.members.length, 3, 'Chain should have 3 members');
+    assert.equal(updatedChain.label, `bid_${distributorUser.address}_${manufacturerUser.address}`, 'Chain label should match')
+    assert.equal(updatedChain.members.length, 3, 'Chain should have 3 members');
   })
 
 })

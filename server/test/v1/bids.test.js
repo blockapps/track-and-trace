9a153caf3ec044a13d2371df3c97905447bd6afe
Tanuj Soni <tanuj@blockapps.net>
2019-04-03 21:03:16 +0530

    [TT-35_es6] Bids E2E test

--- server/test/v1/bids.test.js
+++ server/test/v1/bids.test.js
@@ -1,112 +1,136 @@
-require('dotenv').config();
-require('co-mocha');
+import { fsUtil, parser } from 'blockapps-rest';
+import { assert } from 'chai';
 
-const { rest6: rest, common } = require('blockapps-rest');
-const { assert, config } = common;
-
-const { getEmailIdFromToken, createStratoUser } = require(`${process.cwd()}/helpers/oauth`);
+import oauthHelper from '../../helpers/oauth';
+import { factory } from '../../dapp/asset/asset.factory';
 const { get, post } = require(`${process.cwd()}/test/helpers/rest`);
 const endpoints = require(`${process.cwd()}/api/v1/endpoints`);
 
-const TtRole = rest.getEnums(`${process.cwd()}/${config.dappPath}/ttPermission/contracts/TtRole.sol`).TtRole;
-const assetFactory = require(`${process.cwd()}/${config.dappPath}/asset/asset.factory`);
-const AssetState = rest.getEnums(`${process.cwd()}/${config.dappPath}/asset/contracts/AssetState.sol`).AssetState;
-const AssetEvent = rest.getEnums(`${process.cwd()}/${config.dappPath}/asset/contracts/AssetEvent.sol`).AssetEvent;
-const BidState = rest.getEnums(`${process.cwd()}/${config.dappPath}/bid/contracts/BidState.sol`).BidState;
-const BidEvent = rest.getEnums(`${process.cwd()}/${config.dappPath}/bid/contracts/BidEvent.sol`).BidEvent;
+import { getYamlFile } from '../../helpers/config';
+const config = getYamlFile('config.yaml');
 
-const adminToken = process.env.ADMIN_TOKEN;
-const manufacturerToken = process.env.MANUFACTURER_TOKEN;
-const distributerToken = process.env.DISTRIBUTOR_TOKEN;
+import dotenv from 'dotenv';
 
-const TEST_TIMEOUT = 60000;
+const loadEnv = dotenv.config()
+assert.isUndefined(loadEnv.error)
+
+const adminToken = { token: process.env.ADMIN_TOKEN };
+const manufacturerToken = { token: process.env.MANUFACTURER_TOKEN };
+const distributerToken = { token: process.env.DISTRIBUTOR_TOKEN };
+const regulatorToken = { token: process.env.REGULATOR_TOKEN };
 
 describe('Bids End-To-End Tests', function () {
-  this.timeout(TEST_TIMEOUT);
+  this.timeout(config.timeout);
   let asset, bidsList, bidDetail;
+  let TtRole, AssetState, AssetEvent, BidState, BidEvent
 
-  const createUser = function* (userToken, role) {
+  // TODO: move in utils
+  const createUser = async function (userToken, role) {
     try {
-      const user = yield get(endpoints.Users.me, userToken);
+      const user = await get(endpoints.Users.me, userToken.token);
       assert.equal(user.role, role, 'user already created with different role');
     } catch (err) {
       console.log(err);
-      const userEmail = getEmailIdFromToken(userToken);
-      const createAccountResponse = yield createStratoUser(userToken, userEmail);
+      const userEmail = oauthHelper.getEmailIdFromToken(userToken.token);
+      const createAccountResponse = await oauthHelper.createStratoUser(userToken, userEmail);
       const createTtUserArgs = {
         account: createAccountResponse.address,
         username: userEmail,
         role: role
       };
-      yield post(endpoints.Users.users, createTtUserArgs, adminToken);
+      await post(endpoints.Users.users, createTtUserArgs, adminToken.token);
     }
   }
 
-  before(function* () {
+  before(async function () {
+    // TODO: check this properly
     assert.isDefined(adminToken, 'admin token is not defined');
     assert.isDefined(manufacturerToken, 'manufacturer token is not defined');
     assert.isDefined(distributerToken, 'distributer token is not defined');
-    yield createUser(manufacturerToken, TtRole.MANUFACTURER);
-    yield createUser(distributerToken, TtRole.DISTRIBUTOR);
 
-    const createAssetArgs = assetFactory.getAssetArgs();
-    asset = yield post(endpoints.Assets.assets, { asset: createAssetArgs }, manufacturerToken);
+    // get ttRole Enums
+    const ttRoleSource = fsUtil.get(`${process.cwd()}/${config.dappPath}/ttPermission/contracts/TtRole.sol`)
+    TtRole = await parser.parseEnum(ttRoleSource);
+
+    // get assetState Enums
+    const assetStateSource = fsUtil.get(`${process.cwd()}/${config.dappPath}/asset/contracts/AssetState.sol`)
+    AssetState = await parser.parseEnum(assetStateSource);
+
+    // get AssetEvent Enums
+    const assetEventSource = fsUtil.get(`${process.cwd()}/${config.dappPath}/asset/contracts/AssetEvent.sol`)
+    AssetEvent = await parser.parseEnum(assetEventSource);
+
+    // get BidState Enums
+    const bidStateSource = fsUtil.get(`${process.cwd()}/${config.dappPath}/bid/contracts/BidState.sol`)
+    BidState = await parser.parseEnum(bidStateSource);
+
+    // get BidEvent Enums
+    const bidEventSource = fsUtil.get(`${process.cwd()}/${config.dappPath}/bid/contracts/BidEvent.sol`)
+    BidEvent = await parser.parseEnum(bidEventSource);
+
+    await createUser(manufacturerToken, TtRole.MANUFACTURER);
+    await createUser(distributerToken, TtRole.DISTRIBUTOR);
+    await createUser(regulatorToken, TtRole.REGULATOR);
+
+    const createAssetArgs = factory.getAssetArgs();
+    asset = await post(endpoints.Assets.assets, { asset: createAssetArgs }, manufacturerToken.token);
     assert.equal(asset.sku, createAssetArgs.sku);
 
     const handleEventUrl = `${endpoints.Assets.assets}/handleEvent`;
-    const assetState = yield post(handleEventUrl, { sku: asset.sku, assetEvent: AssetEvent.REQUEST_BIDS }, manufacturerToken);
+    const assetState = await post(handleEventUrl, { sku: asset.sku, assetEvent: AssetEvent.REQUEST_BIDS }, manufacturerToken.token);
     assert.equal(AssetState.BIDS_REQUESTED, assetState, "State should be updated");
   });
 
-  it('Create Bid', function* () {
+  it('Create Bid', async function () {
     const bidValue = 10;
-    bidDetail = yield post(endpoints.Bids.bids, { address: asset.address, owner: asset.owner, bidValue }, distributerToken);
+    const regulatorEmail = oauthHelper.getEmailIdFromToken(regulatorToken.token);
+    bidDetail = await post(endpoints.Bids.bids, { address: asset.address, owner: asset.owner, bidValue, regulatorEmail }, distributerToken.token);
     assert.equal(bidDetail.assetOwner, asset.owner, "owner address should be same");
     assert.equal(bidDetail.asset, asset.address, "asset address should be same");
     assert.equal(bidDetail.value, bidValue, "bid value should be same");
   });
 
-  it('List bids', function* () {
-    bidsList = yield get(endpoints.Bids.bids, manufacturerToken);
+  it('List bids', async function () {
+    bidsList = await get(endpoints.Bids.bids, manufacturerToken.token);
     assert.isAtLeast(bidsList.length, 1, 'bids should be at least 1');
   });
 
-  it('Get bids using address', function* () {
+  it('Get bids using address', async function () {
     const url = `${endpoints.Bids.bids}/${bidsList[0].address}`;
-    const bidDetail = yield get(url, manufacturerToken);
+    const bidDetail = await get(url, manufacturerToken.token);
     assert.isDefined(bidDetail, 'bid detail using address');
   });
 
-
-  it('ACCEPT - handle event', function* () {
+  it('ACCEPT - handle event', async function () {
     const url = `${endpoints.Bids.bids}/${bidDetail.address}/event`;
-    const bidState = yield post(url, { chainId: bidDetail.chainId, bidEvent: BidEvent.ACCEPT }, manufacturerToken);
+    const bidState = await post(url, { chainId: bidDetail.chainId, bidEvent: BidEvent.ACCEPT }, manufacturerToken.token);
     assert.equal(bidState, BidState.ACCEPTED, "bid state should be in Accepted state");
   });
 
-  it('REJECT - handle event', function* () {
+  it('REJECT - handle event', async function () {
     // create bid
     const bidValue = 10;
-    const detail = yield post(endpoints.Bids.bids, { address: asset.address, owner: asset.owner, bidValue }, distributerToken);
+    const regulatorEmail = oauthHelper.getEmailIdFromToken(regulatorToken.token);
+    const detail = await post(endpoints.Bids.bids, { address: asset.address, owner: asset.owner, bidValue, regulatorEmail }, distributerToken.token);
     assert.equal(detail.assetOwner, asset.owner, "owner address should be same");
     assert.equal(detail.asset, asset.address, "asset address should be same");
     assert.equal(detail.value, bidValue, "bid value should be same");
 
     // Reject bid
     const url = `${endpoints.Bids.bids}/${detail.address}/event`;
-    const bidState = yield post(url, { chainId: detail.chainId, bidEvent: BidEvent.REJECT }, manufacturerToken);
+    const bidState = await post(url, { chainId: detail.chainId, bidEvent: BidEvent.REJECT }, manufacturerToken.token);
     assert.equal(bidState, BidState.REJECTED, "bid state should be in Rejected state");
   });
 
-  it('should retrieve asset and bid history', function* () {
-    const retrieved = yield get(
-      endpoints.Assets.asset.replace(':sku',asset.sku),
-      manufacturerToken
+  it('should retrieve asset and bid history', async function () {
+    const retrieved = await get(
+      endpoints.Assets.asset.replace(':sku', asset.sku),
+      manufacturerToken.token
     )
 
     assert.equal(retrieved.sku, asset.sku, 'SKU should match');
     assert.isArray(retrieved.history, 'History should be present');
-    assert.isAtLeast(retrieved.history.length,1, 'History should be present');
+    assert.isAtLeast(retrieved.history.length, 1, 'History should be present');
   });
 
 });

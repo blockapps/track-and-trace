00f1a3b9b1f10f6461887d4a3f6fa4c989352d9b
Samrit Sangal <samrit@samrit.io>
2019-05-07 12:34:27 -0400

    Add TODO statements

--- server/api/middleware/asyncHandler.js
+++ server/api/middleware/asyncHandler.js
@@ -1,5 +1,6 @@
-const handler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next)
-}
+// TODO: this is likely not necessary
+const handler = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
 
 export default handler;
--- server/helpers/config.js
+++ server/helpers/config.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
-
+import fs from "fs";
+import yaml from "js-yaml";
+// TODO: All of this should be in blockapps-rest
 // read a yaml or die
 function getYamlFile(yamlFilename) {
-  return yaml.safeLoad(fs.readFileSync(yamlFilename, 'utf8'));
+  return yaml.safeLoad(fs.readFileSync(yamlFilename, "utf8"));
 }
 
 function yamlSafeDumpSync(object) {
@@ -12,16 +12,11 @@ function yamlSafeDumpSync(object) {
 
 function yamlWrite(object, filename) {
   const yaml = yamlSafeDumpSync(object);
-  writeFileSync(filename, yaml, 'utf8');
+  writeFileSync(filename, yaml, "utf8");
 }
 
 function writeFileSync(file, data, options) {
   fs.writeFileSync(file, data, options);
 }
 
-export {
-  getYamlFile,
-  yamlSafeDumpSync,
-  yamlWrite,
-  writeFileSync
-};
+export { getYamlFile, yamlSafeDumpSync, yamlWrite, writeFileSync };

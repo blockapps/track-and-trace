f279ea7c5cbc326687ac5eeeeaf54d3d399449c2
Lior Saar <liorsaar1@gmail.com>
2019-07-26 08:36:02 -0700

    dapp

--- server/index.js
+++ server/index.js
@@ -48,6 +48,7 @@ app.use(`${constants.baseUrl}`, routes);
 
 app.use(errorHandler)
 
+console.log('>>>>>>>>>>>> index.js')
 const port = process.env.PORT || 3030;
 
 const server = app.listen(port, () => console.log(`Listening on ${port}`));

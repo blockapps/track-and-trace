0d76fcdc807a93ce7e45f2e4b7f2f5d5ea6b4d5f
Naveen <naveen_vijayhotmail.com>
2019-07-31 10:10:48 -0400

    Download and Verify tests for External Storage have been added.

--- server/dapp/exstorage/test/exstorage.test.js
+++ server/dapp/exstorage/test/exstorage.test.js
@@ -34,7 +34,7 @@ describe('External Storage Tests', function () {
     const users = await rest.getUsers({}, options)
     const index = users.indexOf(adminBlocName)
     assert.isAtLeast(index, 0, 'user not found: ' + adminBlocName)
-    adminBlocAddress = await rest.getUser({username: adminBlocName}, options)
+    adminBlocAddress = await rest.getUser({ username: adminBlocName }, options)
     assert.isDefined(adminBlocAddress, 'adminBlocAddress')
   });
 
@@ -71,6 +71,7 @@ describe('External Storage Tests', function () {
       content,
       type,
     }
+
     const results = await upload(args)
     // {
     // "contractAddress":"8f8fa44ea0a84a7b4dc9f64fb85e77f3583d65a9",
@@ -80,6 +81,70 @@ describe('External Storage Tests', function () {
     assert.isDefined(results.contractAddress, 'contractAddress')
     assert.isDefined(results.uri, 'uri')
     assert.equal(results.metadata, args.metadata, 'metadata')
+  });
+
+  it('Download file', async function () {
+
+    const uid = util.uid()
+    const content = `${util.cwd}/${config.dappPath}/exstorage/test/fixtures/upload.jpg`
+    const type = 'image/jpeg'
+
+    const uploadArgs = {
+      host: 'localhost:8080',
+      username: adminBlocName,
+      password: adminBlocPassword,
+      address: adminBlocAddress,
+      provider: 's3',
+      metadata: `metadata ${uid}`,
+      content,
+      type,
+    }
+    const uploadResults = await upload(uploadArgs)
+    const args = {
+      host: 'localhost:8080',
+      contractAddress: uploadResults.contractAddress
+    }
+
+    const downloadResults = await download(args)
+    /*
+    {"url":"https://strato-external-test-bucket.s3.amazonaws.com/1564526045712-upload.jpg?AWSAccessKeyId=AKIASUITSAQO7UEK2EN5&Expires=1564583770&Signature=2iET3vts2MRqpgfCTURpyPjtAiU%3D"}
+    */
+    assert.isDefined(downloadResults.url, 'url')
+  });
+
+
+  it('Verify file', async function () {
+    const uid = util.uid()
+    const content = `${util.cwd}/${config.dappPath}/exstorage/test/fixtures/upload.jpg`
+    const type = 'image/jpeg'
+
+    const uploadArgs = {
+      host: 'localhost:8080',
+      username: adminBlocName,
+      password: adminBlocPassword,
+      address: adminBlocAddress,
+      provider: 's3',
+      metadata: `metadata ${uid}`,
+      content,
+      type,
+    }
+    const uploadResults = await upload(uploadArgs)
+
+    const verifyArgs = {
+      host: 'localhost:8080',
+      contractAddress: uploadResults.contractAddress,
+      address: adminBlocAddress,
+    }
+
+    const verifyResults = await verify(verifyArgs)
+    /*
+    {"uri":"https://strato-external-test-bucket.s3.amazonaws.com/1564526045712-upload.jpg",
+    "timeStamp":1564526046,
+    "signers":["71e99ad9d89ad199a1516cb726ebec0dc73b7563"]}
+    */
+    assert.isDefined(verifyResults.uri, 'uri')
+    assert.isDefined(verifyResults.timeStamp, 'timeStamp')
+    assert.equal(verifyResults.signers[0], verifyArgs.address, 'signers')
   });
 });
 
@@ -93,17 +158,42 @@ async function upload(args) {
     -F "provider=s3" \
     -F "metadata=${args.metadata}" \
     -F "content=@${args.content};type=${args.type}"`
-  // console.log('===================== upload')
-  // console.log(command)
   const resultsString = await exec(command)
   return JSON.parse(resultsString)
 }
+
+
+async function download(args) {
+  /*
+  curl -X GET "http://<your-ip-address>/apex-api/bloc/file/download?contractAddress=<contract-address-of-externally-stored-object>"
+  -H "accept: application/json;charset=utf-8"
+  */
+  const command = `curl \
+    -X GET "http://${args.host}/apex-api/bloc/file/download?contractAddress=${args.contractAddress}" \
+    -H "accept: application/json;charset=utf-8"`
+  const resultsString = await exec(command)
+  return JSON.parse(resultsString)
+}
+
+async function verify(args) {
+  /*
+  curl -X GET "http://<your-ip-address>/apex-api/bloc/file/verify?contractAddress=<contract-address-of-externally-stored-object>"
+       -H "accept: application/json;charset=utf-8"
+  */
+  const command = `curl \
+    -X GET "http://${args.host}/apex-api/bloc/file/verify?contractAddress=${args.contractAddress}" \
+  -H "accept: application/json;charset=utf-8"`
+  const resultsString = await exec(command)
+  return JSON.parse(resultsString)
+}
+
 
 async function ls(args) {
   const command = `ls ${args}`
   const results = await exec(command)
   return results
 }
+
 
 async function exec(command) {
   const child = require('child_process')

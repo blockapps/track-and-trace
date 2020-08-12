7e1221b27c2aef45083f9ac4ddb53225e4fbed2d
Samrit Sangal <samrit@samrit.io>
2020-08-12 11:19:47 -0400

    Remove unused dependencies from create asset component

--- ui/src/scenes/Asset/Create/index.js
+++ ui/src/scenes/Asset/Create/index.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, IconButton, Icon, AppBar, Tabs, Tab, Typography, Select, MenuItem } from '@material-ui/core';
-import { reduxForm, Form, Field, FieldArray } from 'redux-form'
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  IconButton,
+  Icon,
+  Select,
+  MenuItem
+} from "@material-ui/core";
+import { reduxForm, Form, Field, FieldArray } from "redux-form";
 import ReduxedTextField from "../../../components/ReduxedTextField";
 import {
   openCreateAssetOverlay,
@@ -10,37 +20,40 @@ import {
   createAsset,
   getAssets
 } from "../../../actions/asset.actions";
-import './Create.css'
+import "./Create.css";
 import { CREATE_ASSET_MODES } from "../../../constants";
 
 class CreateAssetModal extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       file: null,
       createAssetMode: CREATE_ASSET_MODES.USING_FIELDS
-    }
+    };
   }
 
-  onFileLoad = (e) => {
+  onFileLoad = e => {
     this.setState({ file: e.target.files[0] });
-  }
+  };
 
-  submit = (asset) => {
+  submit = asset => {
     const { specs } = asset;
     const { file, createAssetMode } = this.state;
 
-    const params = this.state.createAssetMode === CREATE_ASSET_MODES.USING_FIELDS ? {
-      sku: asset.sku,
-      description: asset.description,
-      name: asset.name,
-      price: asset.price,
-      keys: specs.map(s => s.key),
-      values: specs.map(s => s.value)
-    } : { file };
+    const params =
+      this.state.createAssetMode === CREATE_ASSET_MODES.USING_FIELDS
+        ? {
+            sku: asset.sku,
+            description: asset.description,
+            name: asset.name,
+            price: asset.price,
+            keys: specs.map(s => s.key),
+            values: specs.map(s => s.value)
+          }
+        : { file };
 
     this.props.createAsset(params, createAssetMode);
-  }
+  };
 
   componentDidUpdate(prevProps) {
     if (prevProps.isOpen && !this.props.isOpen && !this.props.error) {
@@ -55,7 +68,7 @@ class CreateAssetModal extends Component {
 
     return (
       <div>
-        {fields.map((user, index) =>
+        {fields.map((user, index) => (
           <div key={index}>
             <div>
               <Field
@@ -78,39 +91,57 @@ class CreateAssetModal extends Component {
                 required
               />
               <span className="spec-delete">
-                {
-                  index ?
-                    <IconButton aria-label="Delete" className="remove-button" onClick={() => fields.remove(index)}>
-                      <Icon color="action">delete</Icon>
-                    </IconButton> : ''
-                }
+                {index ? (
+                  <IconButton
+                    aria-label="Delete"
+                    className="remove-button"
+                    onClick={() => fields.remove(index)}
+                  >
+                    <Icon color="action">delete</Icon>
+                  </IconButton>
+                ) : (
+                  ""
+                )}
               </span>
             </div>
           </div>
-        )}
+        ))}
         <div>
-          <Button variant="contained" color="primary" onClick={() => fields.push({})}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => fields.push({})}
+          >
             add spec
           </Button>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-  handleChange = (event) => {
-    this.setState({ createAssetMode: event.target.value })
+  handleChange = event => {
+    this.setState({ createAssetMode: event.target.value });
   };
 
   render() {
-    const { handleSubmit, isOpen, openCreateAssetOverlay, closeCreateAssetOverlay } = this.props;
+    const {
+      handleSubmit,
+      isOpen,
+      openCreateAssetOverlay,
+      closeCreateAssetOverlay
+    } = this.props;
     const { createAssetMode } = this.state;
 
     return (
       <div>
-        <Button variant="contained" color="primary" onClick={() => {
-          openCreateAssetOverlay();
-          this.props.reset();
-        }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            openCreateAssetOverlay();
+            this.props.reset();
+          }}
+        >
           Create asset
         </Button>
         <Dialog
@@ -126,19 +157,24 @@ class CreateAssetModal extends Component {
                 value={this.state.createAssetMode}
                 onChange={this.handleChange}
               >
-                <MenuItem value={CREATE_ASSET_MODES.USING_FIELDS}>Using fields</MenuItem>
-                <MenuItem value={CREATE_ASSET_MODES.USING_CSV}>Using CSV file</MenuItem>
+                <MenuItem value={CREATE_ASSET_MODES.USING_FIELDS}>
+                  Using fields
+                </MenuItem>
+                <MenuItem value={CREATE_ASSET_MODES.USING_CSV}>
+                  Using CSV file
+                </MenuItem>
               </Select>
-              {createAssetMode === CREATE_ASSET_MODES.USING_FIELDS ?
-                <div><Field
-                  name="sku"
-                  type="text"
-                  placeholder="Stock Keeping Unit"
-                  margin="normal"
-                  component={ReduxedTextField}
-                  fullWidth
-                  required
-                />
+              {createAssetMode === CREATE_ASSET_MODES.USING_FIELDS ? (
+                <div>
+                  <Field
+                    name="sku"
+                    type="text"
+                    placeholder="Stock Keeping Unit"
+                    margin="normal"
+                    component={ReduxedTextField}
+                    fullWidth
+                    required
+                  />
                   <Field
                     name="name"
                     type="text"
@@ -166,36 +202,47 @@ class CreateAssetModal extends Component {
                     fullWidth
                     required
                   />
-                  {
-                    this.props.error && (
-                      <div>error</div>
-                    )
-                  }
+                  {this.props.error && <div>error</div>}
                   <FieldArray name="specs" component={this.renderSpec} />
-                </div> :
+                </div>
+              ) : (
                 <div className="create-asset">
-                  <input type="file" onChange={this.onFileLoad} accept=".csv" required />
-                </div>}
+                  <input
+                    type="file"
+                    onChange={this.onFileLoad}
+                    accept=".csv"
+                    required
+                  />
+                </div>
+              )}
             </DialogContent>
             <DialogActions>
-              <Button onClick={closeCreateAssetOverlay} color="primary"> Cancel </Button>
-              <Button type="submit" color="primary"> {createAssetMode === CREATE_ASSET_MODES.USING_FIELDS ? 'Create' : 'Import'} </Button>
+              <Button onClick={closeCreateAssetOverlay} color="primary">
+                {" "}
+                Cancel{" "}
+              </Button>
+              <Button type="submit" color="primary">
+                {" "}
+                {createAssetMode === CREATE_ASSET_MODES.USING_FIELDS
+                  ? "Create"
+                  : "Import"}{" "}
+              </Button>
             </DialogActions>
           </Form>
         </Dialog>
-      </div >
-    )
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     isOpen: state.asset.isCreateAssetModalOpen,
-    error: state.asset.error,
+    error: state.asset.error
   };
 };
 
-const formed = reduxForm({ form: 'create-asset' })(CreateAssetModal);
+const formed = reduxForm({ form: "create-asset" })(CreateAssetModal);
 const connected = connect(mapStateToProps, {
   openCreateAssetOverlay,
   closeCreateAssetOverlay,
